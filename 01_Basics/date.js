e8e2f315c191c9cf1b6cb2b56f5e36819c431a4c
Priyanka <73979941+priyanka467@users.noreply.github.com>
2024-05-25 20:16:54 +0000

    time

--- 01_Basics/date.js
+++ 01_Basics/date.js
@@ -1,7 +1,17 @@
 // Dates
 
 let myDate = new Date()
-// console.log(myDate.toString());
+console.log(Date.now())
+// console.log(myDate.toString());// Sat May 25 2024 19:52:14 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toISOString()); // 2024-05-25T19:52:14.128Z
+// console.log(myDate.toJSON());// 2024-05-25T19:52:14.128Z
+// console.log(myDate.toLocaleDateString()); //5/25/2024
+// console.log(myDate.toLocaleString()); // 5/25/2024, 7:53:45 PM
+// console.log(myDate.toTimeString()); // 19:54:44 GMT+0000 (Coordinated Universal Time)
+// console.log(myDate.toUTCString()) // Sat, 25 May 2024 19:55:20 GMT
+// console.log(myDate.getTimezoneOffset()) // 0
+
+
 // console.log(myDate.toDateString());
 // console.log(myDate.toLocaleString());
 // console.log(typeof myDate);
@@ -19,9 +29,9 @@ let myTimeStamp = Date.now()
 // console.log(Math.floor(Date.now()/1000));
 
 let newDate = new Date()
-console.log(newDate);
-console.log(newDate.getMonth() + 1);
-console.log(newDate.getDay());
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
 
 // `${newDate.getDay()} and the time `
 

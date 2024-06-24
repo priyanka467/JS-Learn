09c6a21120c255cfb3261ae511b38350f9cf2d1c
Priyanka <73979941+priyanka467@users.noreply.github.com>
2024-06-24 06:27:30 +0000

    filter

---
+++ 05_Iterations/6_six.js
@@ -0,0 +1,13 @@
+// for each, filter 
+const library = ['science', 'maths', 'Computer', 'SST', 'Hindi', 'English', 'Drawing', 'maths', 'maths' ]
+const filterArrMaths = library.filter((key) => {return key === 'maths'})
+console.log(filterArrMaths);
+
+
+
+
+
+
+
+
+
